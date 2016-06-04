1000bcacb0f11fe0a457073377b2e98b21d34a56
TF Lesterhuis <timo.lesterhuis@ricardo.com>
2016-06-04 15:24:32 +0200

    updated some requirements

--- bonkiebot.py
+++ bonkiebot.py
@@ -6,7 +6,6 @@ use python structures:
 - dataset (easy sql connectivity)
 * some form of testing library
 
-ik voeg een nieuqwe line toe
 """
 
 
@@ -30,65 +29,6 @@ TOKEN = tokens.BonkieBot
 
 # CLASSES
 
-
-class Oefening:
-
-    def __init__(self, naam):
-        """
-        DocString
-        """
-        self.naam = naam
-        self.series = []
-        self.opmerking = []
-        
-    def __str__(self):
-        return "{}\n{}\n\n{}"
-    
-    def nieuwe_set(self, reps, gewicht):
-        """
-        DocString
-        """
-        self.series.append(Set(reps, gewicht))
-        
-
-    def opmerking(self, tekst):
-        """
-        DocString
-        """
-        self.opmerking.append(tekst)
-        
-
-class SuperSet:
-    
-    def __init__():
-        """
-        DocString
-        """
-        return
-
-class Set:
-    
-    def __init__(self, herhalingen, gewicht):
-        """
-        DocString
-        """
-        self.herhalingen = herhalingen
-        self.gewicht = gewicht
-        
-        
-    def __str__(self):
-        return 'SET'
-    
-    def __repr__(self):
-        return "Set class: ({}, {})".format(self.herhalingen, self.gewicht)
-
-class Training:
-    
-    def __init__():
-        """
-        DocString
-        """
-        return
 
 class BonkieBot:
     """
